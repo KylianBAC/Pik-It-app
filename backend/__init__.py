88a5c1f0b6e0c692a6ed5b5ca47df79e8e19cd63
KylianBAC <kylian.baconnet@epitech.digital>
2025-03-21 12:07:13 +0100

    [UPD] Correction de bugs

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,7 +3,7 @@ from .database import db
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://user:password@localhost/dbname"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Kykypgadmin@localhost:5432/pikit_pg_db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
