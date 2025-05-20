63b0a718b4cd3893b162d1e6785479c942d22a5b
KylianBAC <kylian.baconnet@epitech.digital>
2025-05-20 10:38:33 +0200

    [UPD] fix ip api

--- pik-it-react-native-app/api/auth.js
+++ pik-it-react-native-app/api/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://10.134.200.34:5000';
+const API_URL = 'http://10.134.200.98:5000';
 // const API_URL = 'http://192.168.1.224:5000'; 
 
 // Enregistrer un nouvel utilisateur
